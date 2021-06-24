39e94fe60132b21f42ea0a79c1efa05173a27161
hntoma <hntoma@gmail.com>
2021-06-24 09:31:03 -0700

    welcome page

---
+++ welcome.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+
+<?php 	
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Welcome page!</title>
+</head>
+<body>
+<center>
+	<h1>Welcome</h1>
+	
+</center>
+</body>
+</html>
